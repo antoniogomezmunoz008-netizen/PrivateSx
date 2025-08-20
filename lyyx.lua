532a5ad2e075c1e8c8215bc5a5674aba0263b749
antoniogomezmunoz008-netizen <antoniogomezmunoz008@gmail.com>
2025-08-20 07:25:10 +0200

    Update lyyx.lua

--- lyyx.lua
+++ lyyx.lua
@@ -46,13 +46,42 @@ MiscSection:NewButton("Destroy Ad Teleport", "Elimina portales de publicidad", f
     if part then part:Destroy() end
 end)
 
--- Caminar sobre el agua
+-- Caminar sobre el agua (optimizando todas tus posiciones)
 MiscSection:NewButton("Walk On Water", "Crear plataformas invisibles", function()
     local posiciones = {
         Vector3.new(11, -9, 78),
         Vector3.new(-110, -9, -999),
         Vector3.new(-110, -9, 1999),
+        Vector3.new(-110, -9, -1999),
+        Vector3.new(-110, -9, 2999),
+        Vector3.new(-110, -9, -3999),
+        Vector3.new(-110, -9, 4999),
+        Vector3.new(-110, -9, -5999),
+        Vector3.new(-110, -9, -6999),
+        Vector3.new(-110, -9, -7999),
+        Vector3.new(-110, -9, -8999),
+        Vector3.new(-110, -9, -9999),
+        Vector3.new(-8922.48, -9.84, -6233.65),
+        Vector3.new(-3072.01, -9.64, -6136.02),
+        Vector3.new(-5100.05, -9.21, -5128.03),
+        Vector3.new(-7096.39, -9.78, -6403.66),
+        Vector3.new(-9882.70, -9.63, -5228.53),
+        Vector3.new(-7888.44, -9.20, -4648.04),
+        Vector3.new(-9873.09, -9.26, -6775.55),
+        Vector3.new(-8453.76, -9.26, -7212.89),
+        Vector3.new(-6483.57, -9.26, -9055.90),
+        Vector3.new(1009.69, -9.11, 1046.96),
+        Vector3.new(2178.02, -9.09, 287.15),
+        Vector3.new(1416.96, -9.90, 2066.50),
+        Vector3.new(1687.23, -9.25, 5069.81),
+        Vector3.new(1655.41, -9.04, 6056.37),
+        Vector3.new(1945.24, -9.39, 8507.65),
+        Vector3.new(2895.56, -9.35, -2546.12),
+        Vector3.new(6886.39, -9.50, -3488.15),
+        Vector3.new(10812.92, -9.64, -3987.93),
+        Vector3.new(14800.76, -9.78, -4368.71),
     }
+
     for _, pos in ipairs(posiciones) do
         local WalkPart = Instance.new("Part")
         WalkPart.Parent = workspace
